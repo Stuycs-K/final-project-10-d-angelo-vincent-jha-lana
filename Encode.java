184d587aab23dde549b73ad8a505a4fa607fca92
Vincent D'Angelo <vdangelo40@stuy.com>
2024-05-22 15:35:30 -0400

    began thinking about encode

---
+++ Encode.java
@@ -0,0 +1,18 @@
+public class Encode{
+
+//args[0] is the first keyword, args[1] is the second keyword, args[2] is the desired message
+  public static void main(String[] args) {
+
+    String part1 = grid(args[0], args[1]);
+  }
+
+  public static String grid(String keyword, String message){
+
+    String result = "";
+    char[][] square = new char[6][6];
+    char[] messageChars = message.toCharArray();
+    for(int i = 0; i < message.length(); i++){
+
+    }
+  }
+}
