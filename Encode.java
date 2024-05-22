f92b54e2426521e171389d2b414a3620e8307ca8
Vincent D'Angelo <vdangelo40@stuy.com>
2024-05-22 17:35:59 -0400

    can delete excess letters from the message

--- Encode.java
+++ Encode.java
@@ -1,9 +1,10 @@
+import java.util.*;
 public class Encode{
 
 //args[0] is the first keyword, args[1] is the second keyword, args[2] is the desired message
   public static void main(String[] args) {
 
-    String part1 = grid(args[0], args[1]);
+    String part1 = grid(args[0], args[2]);
   }
 
   public static String grid(String keyword, String message){
@@ -11,8 +12,28 @@ public class Encode{
     String result = "";
     char[][] square = new char[6][6];
     char[] messageChars = message.toCharArray();
-    for(int i = 0; i < message.length(); i++){
 
+
+    Map<Character,Integer> map = new HashMap<>();
+
+    String newMessage = "";
+    for(int i = 0; i < message.length(); i++){
+      if(!map.containsKey(messageChars[i]))
+      {
+        newMessage += messageChars[i];
+        map.put(messageChars[i], 1);
+      }
     }
+
+    System.out.println(newMessage);
+
+
+
+
+
+
+
+    return result;
+
   }
 }
