66fa988e7560fe210e8af13faea4b9b25ce4fbd2
Shwetlana Jha <sjha40@stuy.edu>
2024-06-07 15:20:45 -0400

    cleaned up code and added comments

--- decode.py
+++ decode.py
@@ -19,43 +19,37 @@ create a string that goes thro the polybius square to find the message - method:
 
 return string
 '''
+
 '''Takes arguments from decode and returns the final message'''
 def decode(keyword1,keyword2,encoded_message):
     array_of_columns = encoded_message.split()
-    # print(array_of_columns)
     arranged_columns = rearrange_columns(array_of_columns, keyword2)
-    # print(arranged_columns)
     pairs = get_pairs(arranged_columns)
     polybius_square = create_square(keyword1)
     message = get_message(polybius_square,pairs)
     return(message)
-    # return("hi")
 
+'''Takes the orignal columns and the second keyword as arguments and returns an array with the columns rearranged into their original format(non alphabetically)'''
 def rearrange_columns(original_columns, keyword2):
-    alphabetized = ''.join(sorted(keyword2))
-    # print(alphabetized)
-    # print(alphabetized)
-    position_array=[]
+    alphabetized = ''.join(sorted(keyword2)) #puts the keyword in alphabetical order
+    position_array=[] #holds the index of where each column should be in accordance to the original keyword
 
     for i in range(len(alphabetized)):
-        # print("position array:")
-        # print(position_array)
         count = 0
         counter = True
         while counter==True:
-            if alphabetized[i] == keyword2[count] and (i == 0 or count != position_array[i-1]):
+            if alphabetized[i] == keyword2[count] and (i == 0 or count != position_array[i-1]):#accounts for if there are duplicate letters in the keyword
                 position_array.append(count)
                 counter = False
             else:
                 count+=1
 
-    # print(position_array)
     rearranged = ['empty']*len(position_array)
-    for i in range(len(position_array)):
-        # print(i)
+    for i in range(len(position_array)):#uses the indexes from position array to fill up the array with the rearranged columns
         rearranged[position_array[i]]= original_columns[i]
     return rearranged
 
+'''Takes the arranged columns as an argument and returns pairs of letters that can be decoded using the polybius square'''
 def get_pairs(arranged_columns):
     pairs = []
     length = len(arranged_columns[0])
@@ -74,6 +68,7 @@ def get_pairs(arranged_columns):
         
     return pairs
 
+'''Takes the first keyword as an argument and returns an array that represents the 6x6 polybius square'''
 def create_square(keyword1):
     keyword = "".join(dict.fromkeys(keyword1.replace(" ","").lower()))#removes spaces from keyword and duplicated
    
@@ -88,9 +83,11 @@ def create_square(keyword1):
         if count <1:
             final_string += total_string[i]
    
-    # print(final_string)
+
     #final_string has rest of alphabet
-    polybius_square = [""]*6#creates a 6x6 double array
+    polybius_square = [""]*6#creates an array
+
+    #fills the array so that the 6x6 square contains the keyword + the rest of the alphabet
     for i in range(math.floor(len(keyword)/6)):
         polybius_square[0] = keyword[0:6]
     
@@ -99,22 +96,20 @@ def create_square(keyword1):
         polybius_square[math.floor(len(keyword)/6)] = keyword[math.floor(len(keyword)/6)*6:]
         polybius_square[math.floor(len(keyword)/6)] += final_string[0:6 - mod]
     final_string = final_string[6-mod:]
-    # print(final_string)
+
     final_rows=[]
-    # print(polybius_square)
-    #splitting final_string into array of 6's
+
     for x in range(0,len(final_string),6):
         final_rows.append(final_string[x:x+6])
 
-    # print(final_rows)
     for i in range(math.ceil(len(keyword)/6),6,1):
         polybius_square[i] = final_rows[i-math.ceil(len(keyword)/6)]
 
     return polybius_square
 
+'''takes the polybius square and the pairs as an argument and returns the message by converting the letters in the pairs to their orginal letter'''
 def get_message(square, pairs):
-    '''create list of rows,columns from pairs
-    get message  by looping'''
+     #create list of rows,columns from pairs and get message by looping
     indexes = []
     message = ""
     for p in pairs:
@@ -123,6 +118,7 @@ def get_message(square, pairs):
         message+=square[int(i[0])][int(i[1])]
     return message
 
+'''takes a letter and returns the index of where it is located in the polybius square'''
 def convert(letter):
     if letter == 'A':
         return 0
@@ -136,6 +132,8 @@ def convert(letter):
         return 4
     elif letter == 'X':
         return 5
+
+#takes arguments from terminal command, calls the decode function, and prints the final message
 if len(sys.argv) == 5:
     if sys.argv[1] == "decode":
         keyword1 = sys.argv[2]
